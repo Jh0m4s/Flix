ba1b5af59d12d16257fb8518fd29ed1a7ba6eb80
Joey Thomas <joeythomas@Joeys-iMac.lan>
2020-09-19 23:04:14 -0700

    updated with Superhero tab as well as the option to click on a movie to view it's details

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Joey Thomas on 9/19/20.
+//  Copyright © 2020 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- Flix/ViewController.swift
+++ Flix/ViewController.swift
@@ -72,6 +72,29 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         return cell
     }
+    
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        
+        //find selected movie
+        let cell = sender as! UITableViewCell
+        
+        let indexPath = tableView.indexPath(for: cell)!
+        
+        let movie = movies[indexPath.row]
+        
+        let detailsViewControler = segue.destination as! MovieDetailsViewController
+        
+        detailsViewControler.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 
 
 }
